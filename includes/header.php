ef0c2f6767dc68b475ec28a44fea75a1e32f64d0
Jaime Berrios <bo0310042024@unab.edu.sv>
2026-06-09 06:31:41 -0600

    Favicon agregado

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Omning</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     
+    <link rel="icon" type="image/x-icon" href="/omning/assets/img/favicon.ico">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/omning/assets/css/style.css">
 </head>
 <body>
